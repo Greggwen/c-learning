487fea3daa08b462bb317be5e230e0dff72d1fe5
Greggwen <simple.xull@gmail.com>
2019-11-06 20:49:26 +0800

    feat($enum): enum practise

--- example/enum.c
+++ example/enum.c
@@ -1,7 +1,24 @@
 #include <stdio.h>
 
 enum DAY {
-    Mon, Tue, Wed, Thu, Fri, Sat, Sun
+    Mon=1, Tue, Wed, Thu, Fri, Sat, Sun
 };
 
+enum DAY2 {
+    MON=1, TUE, WEN, THU, FRI, SAT, SUN
+} day2;
 
+int main () {
+
+    enum DAY day;
+    day = Wed;
+
+    printf("%d", day);
+
+    
+    for (day2 = MON; day2 <= SUN; day2++) {
+        printf("枚举元素：%d\n", day2);
+    }
+
+    return 0;
+}
